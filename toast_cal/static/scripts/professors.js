a89c06ba7ffc4dee7e846987beb709af2913040f
young1004 <38642032+young1004@users.noreply.github.com>
2020-05-28 15:08:05 +0900

    학생기능 통합 전 백업

--- toast_cal/static/scripts/professors.js
+++ toast_cal/static/scripts/professors.js
@@ -11,6 +11,25 @@ calProBtn.addEventListener('click', function(event) {
 
 subProBtn.addEventListener('click', function(event) {
     changeContents('professor1', 'calendar-common', 'professor2', 'sidebar', 'professor3');
+
+
+
+
+
+    //select 박스 학과 불러오기
+    ajaxPost("/toast_cal/department/", "json", "POST", 1)
+        .then(function(data) {
+            $('#lec-depart').empty(); //기존 옵션 값 삭제
+            // $("#lec-depart").find("option").end().append("<option value='전체'>전체</option>");
+
+            for (var count = 0; count < data.length; count++) {
+                var option = $("<option>" + data[count].fields.name + "</option>");
+                $('#lec-depart').append(option);
+            }
+        })
+        .catch(function(err) {
+            alert(err);
+        });
 });
 
 voteProBtn.addEventListener('click', function(event) {
@@ -159,20 +178,31 @@ $(document).on("click", ".voteBtn", function() {
 
 });
 
+// 강의 개설 버튼
 var lecMakeBtn = document.getElementById("lecMakeBtn");
 
+// 강의 개설 버튼 eventListener
 lecMakeBtn.addEventListener('click', async function(event) {
+
+    let periodData = document.getElementById('lec-type1').value +
+        document.getElementById('lec-type2').value + " " +
+        document.getElementById('lec-type3').value +
+        document.getElementById('lec-type4').value;
+
     let makeData = {
         name: document.getElementById("lec-name").value,
         code: document.getElementById("lec-code").value,
         codeClass: document.getElementById("lec-class").value,
-        period: document.getElementById("lec-time").value,
-        lecture_type: document.getElementById("lec-type").value,
+        period: periodData,
+        lecture_type: document.getElementById('lec-type').value,
         department: document.getElementById("lec-depart").value,
     };
     document.getElementById("make-lecture-wrap").reset();
+
+    console.log(makeData);
     let scheduleData;
 
+    // 강의 개설하는 ajax통신 및 반복일정을 생성할 데이터셋 만들기
     await ajaxPost("/toast_cal/makeSubject/", "json", "POST", makeData)
         .then(function(data) {
 
@@ -207,6 +237,7 @@ lecMakeBtn.addEventListener('click', async function(event) {
         scheduleData
     };
 
+    // 캘린더 일정에 반복적으로 일정 추가
     await ajaxPost("/toast_cal/makeCalendars/", "json", "POST", scheduleData)
         .then(function(data) {
             // alert(data);
@@ -215,6 +246,7 @@ lecMakeBtn.addEventListener('click', async function(event) {
             alert(err);
         });
 
+    // 추가된 반복일정을 캘린더에 뿌려주기
     ajaxPost("/toast_cal/ourstores/", 'json', "POST", "1")
         .then(function(data) {
             calendar.clear();
