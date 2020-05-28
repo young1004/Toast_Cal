2cd250a5b9f10cc5b248330a0c63b4623f21888d
young1004 <38642032+young1004@users.noreply.github.com>
2020-05-28 15:58:00 +0900

    통합본 일부 버그등 수정

--- toast_cal/urls.py
+++ toast_cal/urls.py
@@ -5,10 +5,6 @@ from toast_cal import views, crud
 app_name = "toast_cal"
 
 urlpatterns = [
-    path("changePw/", views.changePw, name="changePw"),  # 회원정보 관련 기능들
-    path("findId/", views.findId, name="findId"),
-    path("findInfo/", views.findInfo, name="findInfo"),
-    path("findPass/", views.findPass, name="findPass"),
     path("", views.calendar, name="calendar"),  # 로그인시 메인 화면
     # 캘린더 기능들
     path("ourstores/", crud.ourstores, name="ourstores"),
@@ -16,7 +12,8 @@ urlpatterns = [
     path("create/", crud.createData, name="createData"),
     path("update/", crud.updateData, name="updateData"),
     path("delete/", crud.deleteData, name="deleteData"),
-    path("dateList/", crud.dateList, name="dateList"),  # 우측 네비게이션
+    # 우측 네비게이션
+    path("dateList/", crud.dateList, name="dateList"),
     path("getWeekSchedule/", crud.getWeekSchedule, name="getWeekSchedule"),
     # 학생 기능들
     path("checked/", crud.checked, name="checked"),
@@ -33,7 +30,8 @@ urlpatterns = [
         crud.student_lecture_delete,
         name="student_lecture_delete",
     ),  # 학생 강의 삭제
-    path("voteTable/", crud.voteTable, name="voteTable"),  # 교수 기능들
+    # 교수 기능들
+    path("voteTable/", crud.voteTable, name="voteTable"),
     path("voteChart/", crud.voteChart, name="voteChart"),
     path("makeSubject/", crud.makeSubject, name="makeSubject"),
     path("makeCalendars/", crud.makeCalendars, name="makeCalendars"),
