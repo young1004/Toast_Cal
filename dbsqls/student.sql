9368187369274c32339e82057aa1bb6c3096449a
young1004 <38642032+young1004@users.noreply.github.com>
2020-08-25 15:49:36 +0900

    db sql 파일 텍스트수정

--- dbsqls/student.sql
+++ dbsqls/student.sql
@@ -10,16 +10,16 @@ VALUES("test1", "컴퓨터공학부", "202058001", "테스트1", "test2@test.com
 INSERT INTO student
 VALUES("test2", "컴퓨터공학부", "202058002", "테스트2", "test3@test.com", "pbkdf2_sha256$180000$riS9yMTbwjJp$FOalF8nsvA689zN3r68498w/xa51zX/TnRR/G5N/3g8=", "010-0000-0002");
 INSERT INTO student
-VALUES("test3", "컴퓨터공학부", "202058003", "테스트3", "test2@test.com", "pbkdf2_sha256$180000$riS9yMTbwjJp$FOalF8nsvA689zN3r68498w/xa51zX/TnRR/G5N/3g8=", "010-0000-0003");
+VALUES("test3", "컴퓨터공학부", "202058003", "테스트3", "test4@test.com", "pbkdf2_sha256$180000$riS9yMTbwjJp$FOalF8nsvA689zN3r68498w/xa51zX/TnRR/G5N/3g8=", "010-0000-0003");
 INSERT INTO student
-VALUES("test4", "컴퓨터공학부", "202058004", "테스트4", "test2@test.com", "pbkdf2_sha256$180000$riS9yMTbwjJp$FOalF8nsvA689zN3r68498w/xa51zX/TnRR/G5N/3g8=", "010-0000-0004");
+VALUES("test4", "컴퓨터공학부", "202058004", "테스트4", "test5@test.com", "pbkdf2_sha256$180000$riS9yMTbwjJp$FOalF8nsvA689zN3r68498w/xa51zX/TnRR/G5N/3g8=", "010-0000-0004");
 INSERT INTO student
-VALUES("test5", "컴퓨터공학부", "202058005", "테스트5", "test2@test.com", "pbkdf2_sha256$180000$riS9yMTbwjJp$FOalF8nsvA689zN3r68498w/xa51zX/TnRR/G5N/3g8=", "010-0000-0005");
+VALUES("test5", "컴퓨터공학부", "202058005", "테스트5", "test6@test.com", "pbkdf2_sha256$180000$riS9yMTbwjJp$FOalF8nsvA689zN3r68498w/xa51zX/TnRR/G5N/3g8=", "010-0000-0005");
 INSERT INTO student
-VALUES("test6", "컴퓨터공학부", "202058006", "테스트6", "test2@test.com", "pbkdf2_sha256$180000$riS9yMTbwjJp$FOalF8nsvA689zN3r68498w/xa51zX/TnRR/G5N/3g8=", "010-0000-0006");
+VALUES("test6", "컴퓨터공학부", "202058006", "테스트6", "test7@test.com", "pbkdf2_sha256$180000$riS9yMTbwjJp$FOalF8nsvA689zN3r68498w/xa51zX/TnRR/G5N/3g8=", "010-0000-0006");
 INSERT INTO student
-VALUES("test7", "컴퓨터공학부", "202058007", "테스트7", "test2@test.com", "pbkdf2_sha256$180000$riS9yMTbwjJp$FOalF8nsvA689zN3r68498w/xa51zX/TnRR/G5N/3g8=", "010-0000-0007");
+VALUES("test7", "컴퓨터공학부", "202058007", "테스트7", "test8@test.com", "pbkdf2_sha256$180000$riS9yMTbwjJp$FOalF8nsvA689zN3r68498w/xa51zX/TnRR/G5N/3g8=", "010-0000-0007");
 INSERT INTO student
-VALUES("test8", "컴퓨터공학부", "202058008", "테스트8", "test2@test.com", "pbkdf2_sha256$180000$riS9yMTbwjJp$FOalF8nsvA689zN3r68498w/xa51zX/TnRR/G5N/3g8=", "010-0000-0008");
+VALUES("test8", "컴퓨터공학부", "202058008", "테스트8", "test9@test.com", "pbkdf2_sha256$180000$riS9yMTbwjJp$FOalF8nsvA689zN3r68498w/xa51zX/TnRR/G5N/3g8=", "010-0000-0008");
 INSERT INTO student
-VALUES("test9", "컴퓨터공학부", "202058009", "테스트9", "test2@test.com", "pbkdf2_sha256$180000$riS9yMTbwjJp$FOalF8nsvA689zN3r68498w/xa51zX/TnRR/G5N/3g8=", "010-0000-0009");
+VALUES("test9", "컴퓨터공학부", "202058009", "테스트9", "test10@test.com", "pbkdf2_sha256$180000$riS9yMTbwjJp$FOalF8nsvA689zN3r68498w/xa51zX/TnRR/G5N/3g8=", "010-0000-0009");
